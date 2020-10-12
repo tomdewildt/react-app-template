f82c0dfa91f6306d23caef214c8b2637b0b01f7b
tomdewildt <t.l.dewildt@gmail.com>
2020-10-13 00:05:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
 import * as serviceWorker from "./serviceWorker";
+import store from "./store";
+import App from "./app/App";
 
 ReactDOM.render(
-    <React.StrictMode />,
+    <React.StrictMode>
+        <Provider store={ store }>
+            <App />
+        </Provider>
+    </React.StrictMode>,
     document.getElementById( "root" ),
 );
 
